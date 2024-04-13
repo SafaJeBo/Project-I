bfe673d8f1ef659ab3a5450a820e0539b1676830
Berta B <berta.bobru@gmail.com>
2024-04-13 21:09:50 +0200

    Modified main parallel to account for initialization. MOD_INIT DOES NOT WORK NOW

--- parallel/MOD_INIT.f90
+++ parallel/MOD_INIT.f90
@@ -3,47 +3,66 @@ module MOD_INIT
     implicit none
 
 contains
-    ! INITIALIZE PARTICLES IN A SIMPLE CUBIC CONFIGURATION
-    subroutine do_SCC(Nat, L, r_xyz, nprocs, rank, fileout_scc)
+    ! INITIALIZE PARTICLES IN A SIMPLE CUBIC CONFIGURATION AND DISTRIBUTE PARTICLES !
+    subroutine do_SCC(Nat, L, r_xyz, atoms_list, nprocs, rank, fileout_scc)
         ! Nat: number of atoms
         ! L: size of one side of the simulation box
         ! r_xyz: OUTPUT, position of the particles
+        ! atoms_list: OUTPUT, list of the indexes of the atoms corresponding to the processor
         ! nprocs: number of processors
         ! rank: number associated at the current processor
         ! fileout_scc: name of output file where to print positions
         implicit none
         integer, intent(in) :: Nat, nprocs, rank
+        integer, intent(out), allocatable :: atoms_list(:)
         real(8), intent(in) :: L
         real(8), dimension(:, :), intent(out) :: r_xyz
         character(len=*), intent(in) :: fileout_scc
 
-        integer :: i, j, k, indx, atoms_per_proc, start_atom, end_atom, ierror
+        integer :: M,i, j, k, indx, atoms_per_proc, start_atom, end_atom, ierror
         real(8) :: a
 
         ! Calculate lattice parameter
-        a = L / (Nat ** (1.0d0 / 3.0d0))
+        M=int(Nat**(1.d0/3.d0))
+        a=L/real(M,8)
+
+        if (rank==0) print *, L, M, a
 
         ! Determine number of atoms per proc and assign start and end indices
         atoms_per_proc = Nat / nprocs
         start_atom = rank * atoms_per_proc + 1
         end_atom = start_atom + atoms_per_proc - 1
-        if (rank == nprocs - 1) end_atom = Nat  ! Ensure last proc gets any extra atoms
 
-        ! Calculate positions for this proc's atoms
-        indx = 0
-        do i = 1, nint(Nat ** (1.0d0 / 3.0d0))
-            do j = 1, nint(Nat ** (1.0d0 / 3.0d0))
-                do k = 1, nint(Nat ** (1.0d0 / 3.0d0))
-                    indx = indx + 1
-                    if (indx >= start_atom .and. indx <= end_atom) then
-                        r_xyz(indx, :) = [(i - 1) * a, (j - 1) * a, (k - 1) * a]
-                    end if
-                end do
-            end do
+        if (rank == nprocs - 1) then
+            atoms_per_proc = atoms_per_proc + Nat-end_atom
+            end_atom = Nat  ! Ensure last proc gets any extra atoms    
+        end if
+
+        ! Save indexes in list
+        allocate(atoms_list(atoms_per_proc))
+        indx = 1
+        do i = start_atom, end_atom
+            atoms_list(indx) = i
+            indx = indx + 1
         end do
 
+        ! Calculate positions for this proc's atoms
+            indx = 0
+            do i = 1, M !nint(Nat ** (1.0d0 / 3.0d0))
+                do j = 1, M !nint(Nat ** (1.0d0 / 3.0d0))
+                    do k = 1, M !nint(Nat ** (1.0d0 / 3.0d0))
+                        indx = indx + 1
+                        if (indx >= start_atom .and. indx <= end_atom) then ! save only the atoms associated to processor
+                            r_xyz(indx, :) = [(i - 1) * a, (j - 1) * a, (k - 1) * a]
+                        end if
+                    end do
+                end do
+            end do
+        print *, "I", rank, "finished with my particles", atoms_per_proc
         ! Synchronize before writing to file
         call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+
+        call MPI_ALLGATHER(r_xyz(start_atom:end_atom, :), atoms_per_proc*3, MPI_REAL8, r_xyz, atoms_per_proc*3, MPI_REAL8, MPI_COMM_WORLD, ierror)
 
         if (rank == 0) then
             open(unit=21, file=fileout_scc, status='replace')
@@ -74,7 +93,7 @@ contains
             do j=1,3
                 if (r_xyz(i,j)>L/2.d0) then
                         r_xyz(i,j) = r_xyz(i,j)-dble(L)
-                        
+
                 else if (r_xyz(i,j)<L/2.d0) then
                         r_xyz(i,j) = r_xyz(i,j)+dble(L)
                 endif
--- parallel/main.f90
+++ parallel/main.f90
@@ -1,34 +1,61 @@
 ! main program
 program main
-    ! using the provided modules
-    use initialize
-    use forces
-    use integrate
-    use thermodynamics
+    ! Using the provided modules
+    use mpi
+
+    use MOD_INIT
+    ! use [integration]
+    ! use [force]
+    ! use [thermodynamics]
+
+    ! MPI variables
+    integer :: ierror, rank, nprocs
 
     ! variable declaration
-    implicit none
-    integer,parameter :: d=3!, N=125,nsim_temp=1000,nsim_tot=1000,numdr=1000
+    integer,parameter :: d=3
     integer :: N,nsim_temp,nsim_tot,numdr
     integer :: M,i,j
+    integer, allocatable :: atoms_list(:)
     real(8) :: density,L,a,dt,cutoff,temp1,temp2,nu,sigma,temperatura,ke,pot
     real(8) :: timeini,timefin,msdval,r,deltar,volumdr,pi,press
     real(8), allocatable :: pos(:,:), vel(:,:), pos0(:,:), rdf(:)
     character(15) :: dummy
-    
-    ! Read parameters
-    read(*,*) dummy, N
-    read(*,*) dummy, nsim_temp
-    read(*,*) dummy, nsim_tot
-    read(*,*) dummy, numdr
-    read(*,*) dummy, dt
-    read(*,*) dummy, cutoff
-    read(*,*) dummy, density
-    read(*,*) dummy, temp1
-    read(*,*) dummy, temp2
-    read(*,*) dummy, nu
 
     pi=4d0*datan(1d0)
+    
+    ! ------------------------------------------------------------------ !
+    ! Initialize MPI
+    call MPI_INIT(ierror)
+    call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierror)
+    call MPI_COMM_SIZE(MPI_COMM_WORLD, nprocs, ierror)
+
+    print *, "Hi! I am", rank
+    if (rank.eq.0) then
+        ! Read parameters (only master)
+        read(*,*) dummy, N
+        read(*,*) dummy, nsim_temp
+        read(*,*) dummy, nsim_tot
+        read(*,*) dummy, numdr
+        read(*,*) dummy, dt
+        read(*,*) dummy, cutoff
+        read(*,*) dummy, density
+        read(*,*) dummy, temp1
+        read(*,*) dummy, temp2
+        read(*,*) dummy, nu
+        print *, "I am ", rank, "I finished reading inputs. Going to broadcast"
+    end if
+
+    ! Broadcast values
+    call MPI_BCAST(N, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierror)
+    call MPI_BCAST(nsim_temp, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierror)
+    call MPI_BCAST(nsim_tot, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierror)
+    call MPI_BCAST(numdr, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, ierror)
+    call MPI_BCAST(dt, 1, MPI_REAL8, 0, MPI_COMM_WORLD, ierror)
+    call MPI_BCAST(cutoff, 1, MPI_REAL8, 0, MPI_COMM_WORLD, ierror)
+    call MPI_BCAST(density, 1, MPI_REAL8, 0, MPI_COMM_WORLD, ierror)
+    call MPI_BCAST(temp1, 1, MPI_REAL8, 0, MPI_COMM_WORLD, ierror)
+    call MPI_BCAST(temp2, 1, MPI_REAL8, 0, MPI_COMM_WORLD, ierror)
+    call MPI_BCAST(nu, 1, MPI_REAL8, 0, MPI_COMM_WORLD, ierror)
 
     ! Allocate variables
     allocate(pos(N,d))
@@ -36,79 +63,95 @@ program main
     allocate(pos0(N,d))
     allocate(rdf(numdr))
 
-    ! Opening files to save results
-    open(14,file='trajectory.xyz')
-    open(15,file='thermodynamics.dat')
-    open(16,file='resultsrdflong_def.dat')
+    ! ! Opening files to save results
+    ! open(14,file='trajectory.xyz')
+    ! open(15,file='thermodynamics.dat')
+    ! open(16,file='resultsrdflong_def.dat')
 
+    call MPI_BARRIER(MPI_COMM_WORLD,ierror) ! Barrier to start program at the same time
 
     ! Getting the initial time to account for total simulation time
-    call cpu_time(timeini)
-    write(*,*)timeini
+    if (rank.eq.0) then 
+        call cpu_time(timeini)
+        write(*,*)timeini
+    end if
     
     call srand(1)
 
-    
     ! Initialize system
-    L=(dble(N)/density)**(1.d0/3.d0)
-    M=int(N**(1.d0/3.d0))+1
-    a=L/dble(M)
+    L=(real(N,8)/density)**(1.d0/3.d0)
+    call do_SCC(N, L, pos, atoms_list ,nprocs, rank, "SCCconf_init.xyz")
 
-    print *, L, cutoff, M, a
+    call MPI_BARRIER(MPI_COMM_WORLD,ierror)
+    if (rank.eq.0) then 
+        print *, rank
+        print *, atoms_list
+    end if 
+
+    call MPI_BARRIER(MPI_COMM_WORLD,ierror)
+    if (rank.eq.1) then 
+        print *, rank
+        print *, atoms_list
+    end if 
+
+    ! HERE GOES SEPARATION OF PARTICLES BY PROC
     
-    call ini_pos_sc(N,a,M,d,pos)
-    write(14,*) pos
+    ! !Thermalization
+    ! sigma = sqrt(temp1)
+    ! do i=1,nsim_temp
+    !     call time_step_vVerlet(pos,N,d,L,vel,dt,cutoff,nu,sigma,pot)
+    !     write(14,'(A)') trim(adjustl(''))  ! Blank line
+    !     write(14,'(I0)') N  
+    !     write(14,'(A)') trim(adjustl(''))  ! Blank line
+ 	!     write(14,*) pos
+    ! enddo
+    ! print *, "Finished Thermalization"    
+
+
+    ! !Starting production run
+    ! sigma=sqrt(temp2)
+    ! pos0=pos
+    ! rdf=0d0
     
-    !Thermalization
-    sigma = sqrt(temp1)
-    do i=1,nsim_temp
-        call time_step_vVerlet(pos,N,d,L,vel,dt,cutoff,nu,sigma,pot)
-        write(14,'(A)') trim(adjustl(''))  ! Blank line
-        write(14,'(I0)') N  
-        write(14,'(A)') trim(adjustl(''))  ! Blank line
- 	    write(14,*) pos
-    enddo
-    print *, "Finished Thermalization"    
+    ! do i=1,nsim_tot
+    !     call time_step_vVerlet(pos,N,d,L,vel,dt,cutoff,nu,sigma,pot)
+    !     if (mod(i,100).eq.0) then
+    !         ! Mesure energy, pressure and msd
+    !         call kin_energy(vel,N,d,ke)
+    !         call msd(pos,N,d,pos0,L,msdval)
+    !         call pression(pos,N,d,L,cutoff,press)
+    !         temperatura=temp_inst(ke,N)
+    !         write(15,*)i*dt,ke,pot,pot+ke,temperatura,msdval,press+temperatura*density
 
+    !         if (mod(i,50000).eq.0) then ! Control state of simulation
+    !             print*,i
+    !         endif
 
-    !Starting production run
-    sigma=sqrt(temp2)
-    pos0=pos
-    rdf=0d0
+    !         ! Mesure RDF after a certain timestep
+    !         if (i.gt.1e3) then
+    !             call gr(pos,N,d,numdr,L,rdf)
+    !         endif
+    !     endif
+    !     AQUI PRBLY TOCA POSAR UNA BARRERA
+    ! enddo
     
-    do i=1,nsim_tot
-        call time_step_vVerlet(pos,N,d,L,vel,dt,cutoff,nu,sigma,pot)
-        if (mod(i,100).eq.0) then
-            ! Mesure energy, pressure and msd
-            call kin_energy(vel,N,d,ke)
-            call msd(pos,N,d,pos0,L,msdval)
-            call pression(pos,N,d,L,cutoff,press)
-            temperatura=temp_inst(ke,N)
-            write(15,*)i*dt,ke,pot,pot+ke,temperatura,msdval,press+temperatura*density
+    ! ! Normalization of RDF
+    ! r=0d0
+    ! deltar=0.5d0*L/numdr
+    ! do i=1,numdr
+    !     r=(i-1)*deltar
+    !     volumdr=4d0*pi*((r+deltar/2d0)**3-(r-deltar/2d0)**3)/3d0
+    !     write(16,*)r,rdf(i)/(sum(rdf)*volumdr*density)
+    ! enddo
 
-            if (mod(i,50000).eq.0) then ! Control state of simulation
-                print*,i
-            endif
+    call MPI_BARRIER(MPI_COMM_WORLD,ierror) ! Final barrier to get time
 
-            ! Mesure RDF after a certain timestep
-            if (i.gt.1e3) then
-                call gr(pos,N,d,numdr,L,rdf)
-            endif
-        endif
-    enddo
-    
-    ! Normalization of RDF
-    r=0d0
-    deltar=0.5d0*L/numdr
-    do i=1,numdr
-        r=(i-1)*deltar
-        volumdr=4d0*pi*((r+deltar/2d0)**3-(r-deltar/2d0)**3)/3d0
-        write(16,*)r,rdf(i)/(sum(rdf)*volumdr*density)
-    enddo
-    
-    call cpu_time(timefin)
-    print*,'FINAL time = ',timefin-timeini
-
+    if (rank.eq.0) then
+        call cpu_time(timefin)
+        print*,'FINAL time = ',timefin-timeini
+    end if
+    ! Finalize MPI
+    call MPI_FINALIZE(ierror)
     
     end program main
     
